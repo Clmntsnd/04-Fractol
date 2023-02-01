88e99054b47bedc95a7768771d51ddc9f4f1646e
Clement Senand <loulou@Clements-MacBook-Air.local>
2023-01-31 20:57:19 -0500

    Xfer

--- src/my_keyhook.c
+++ src/my_keyhook.c
@@ -6,7 +6,7 @@
 /*   By: loulou <loulou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 13:12:18 by csenand           #+#    #+#             */
-/*   Updated: 2023/01/31 20:32:12 by loulou           ###   ########.fr       */
+/*   Updated: 2023/01/31 20:54:53 by loulou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,58 +22,34 @@ void	my_keyhook(mlx_key_data_t keydata, t_fractol *frctl)
 		move(keydata.key, frctl);
 	else if (keydata.key == MLX_KEY_I || keydata.key == MLX_KEY_O)
 		change_maxiter(keydata.key, frctl);
+	else if (keydata.key == MLX_KEY_R && keydata.action == MLX_PRESS)
+		fract_init(frctl);
 	else if (keydata.key == MLX_KEY_H && keydata.action == MLX_PRESS)
 		print_help();
 	fract_calc(frctl);
 }
 
-// void	move(keys_t key, t_fractol *frctl)
-// {
-// 	c_size.re = frctl->c_max.re - frctl->c_min.re;
-// 	c_size.im = frctl->c_max.im - frctl->c_min.im;
-// 	if (key == MLX_KEY_UP)
-// 	{
-// 		frctl->c_min.im += c_size.im * 0.05;
-// 		frctl->c_max.im += c_size.im * 0.05;
-// 	}
-// 	else if (key == MLX_KEY_DOWN)
-// 	{
-// 		frctl->c_min.im -= c_size.im * 0.05;
-// 		frctl->c_max.im -= c_size.im * 0.05;
-// 	}
-// 	else if (key == MLX_KEY_LEFT)
-// 	{
-// 		frctl->c_min.re -= c_size.re * 0.05;
-// 		frctl->c_max.re -= c_size.re * 0.05;
-// 	}
-// 	else if (key == MLX_KEY_RIGHT)
-// 	{
-// 		frctl->c_min.re += c_size.re * 0.05;
-// 		frctl->c_max.re += c_size.re * 0.05;
-// 	}
-// }
-
 void	move(keys_t key, t_fractol *frctl)
 {
 	if (key == MLX_KEY_UP)
 	{	
-		frctl->ymin += 30 / frctl->zoom;
-		frctl->ymax += 30 / frctl->zoom;
-	}
-	if (key == MLX_KEY_DOWN)
-	{	
 		frctl->ymin -= 30 / frctl->zoom;
 		frctl->ymax -= 30 / frctl->zoom;
 	}
-	if (key == MLX_KEY_LEFT)
+	if (key == MLX_KEY_DOWN)
 	{	
-		frctl->xmin += 30 / frctl->zoom;
-		frctl->xmax += 30 / frctl->zoom;
+		frctl->ymin += 30 / frctl->zoom;
+		frctl->ymax += 30 / frctl->zoom;
 	}
-	if (key == MLX_KEY_RIGHT)
+	if (key == MLX_KEY_LEFT)
 	{	
 		frctl->xmin -= 30 / frctl->zoom;
 		frctl->xmax -= 30 / frctl->zoom;
+	}
+	if (key == MLX_KEY_RIGHT)
+	{	
+		frctl->xmin += 30 / frctl->zoom;
+		frctl->xmax += 30 / frctl->zoom;
 	}
 	// fract_calc(frctl);
 }
